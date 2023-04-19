08f99c73b308c0c5185c06bb3e7afefb71089717
Jane Benson <jane.benson1994@gmail.com>
2023-04-18 17:24:22 -0700

    Add ability to change grid size

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-const divs = document.querySelectorAll('.row')
 function addColor() {
 	this.classList.add('hovered')
 }
-divs.forEach((el) => {
-	el.addEventListener('mouseover', addColor)
-})
+function createElement(tag, className) {
+	const node = document.createElement(tag)
+	if (className) node.classList.add(className)
+	return node
+}
+
+function addHoverEffect() {
+	const gridNodes = document.querySelectorAll('.row')
+	gridNodes.forEach((el) => {
+		el.addEventListener('mouseover', addColor)
+	})
+}
+
+function createGrid(n = 16) {
+	if (n <= 0 || n > 100) return 'ERROR'
+	// todo remove prior grid from dom
+	const containerNode = createElement('div', 'container')
+	for (let i = 1; i <= n; i++) {
+		const columnNode = createElement('div', 'column')
+		for (let j = 1; j <= n; j++) {
+			const rowNode = createElement('div', 'row')
+			columnNode.appendChild(rowNode)
+		}
+		containerNode.appendChild(columnNode)
+	}
+	document.body.appendChild(containerNode)
+	addHoverEffect()
+}
+
+createGrid()
