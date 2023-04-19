75655a2eb9d0a1814ad8c1e47463b0a0b0555acf
Jane Benson <jane.benson1994@gmail.com>
2023-04-18 17:53:08 -0700

    Create random color on mouseover

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function addColor() {
-	this.classList.add('hovered')
+	const rgb = getRandomColor()
+	this.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
 }
+function getRandomColor() {
+	return [getRandomInt(0, 255), getRandomInt(0, 255), getRandomInt(0, 255)]
+}
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 function createElement(tag, className) {
 	const node = document.createElement(tag)
 	if (className) node.classList.add(className)
@@ -15,8 +23,11 @@ function addHoverEffect() {
 }
 
 function createGrid(n = 16) {
-	if (n <= 0 || n > 100) return 'ERROR'
-	// todo remove prior grid from dom
+	if (n <= 0 || n > 100) throw new Error('Number must be between 1 and 100')
+	const oldContainerNode = document.querySelector('.container')
+	if (oldContainerNode) {
+		document.body.removeChild(oldContainerNode)
+	}
 	const containerNode = createElement('div', 'container')
 	for (let i = 1; i <= n; i++) {
 		const columnNode = createElement('div', 'column')
