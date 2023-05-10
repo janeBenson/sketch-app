8c3aa07954472752e94f20a001438ec18caa55ba
Jane Benson <jane.benson1994@gmail.com>
2023-05-10 09:06:53 -0700

    Make fade-to-black optional

--- script.js
+++ script.js
@@ -3,21 +3,24 @@ const slider = document.querySelector('#slider')
 const sliderOutput = document.querySelector('#slider-output')
 const colorThemesDropdown = document.querySelector('#color-themes')
 const colorThemePreview = document.querySelector('#color-theme-preview')
+const fadeToBlackCheckbox = document.querySelector('#fade-to-black-option')
 
 const defaultGridSize = 16
 let chosenGridSize = defaultGridSize
 let mouseDown = false
 const defaultColorTheme = 'random'
 let chosenColorTheme = defaultColorTheme
+let fadeToBlack = true
 const colorPalettes = {
 	candy: ['rgb(193,202,214)', 'rgb(212,173,207)', 'rgb(242,106,141)', 'rgb(244,156,187)', 'rgb(203,238,243)'],
 	tropical: ['rgb(182,252,213)', 'rgb(181,255,131)', 'rgb(236,252,182)', 'rgb(255,225,131)', 'rgb(255,176,136)'],
 	mocha: ['rgb(120,77,32)', 'rgb(147,100,51)', 'rgb(177,137,95)', 'rgb(232,188,143)', 'rgb(244,208,171)'],
 	celestial: ['rgb(131,24,152)', 'rgb(121,4,112)', 'rgb(176,38,157)', 'rgb(0,23,112)', 'rgb(9,0,73)'],
 	cherryBlossom: ['rgb(251,111,146)', 'rgb(255,143,171)', 'rgb(255,179,198)', 'rgb(255,194,209)', 'rgb(255,229,236)'],
+	lilac: ['rgb(230,215,255)', 'rgb(231,209,255)', 'rgb(225,196,255)', 'rgb(216,185,255)', 'rgb(210,175,255)'],
 	ocean: ['rgb(6,66,115)', 'rgb(118,182,196)', 'rgb(127,205,255)', 'rgb(29,162,216)', 'rgb(222,243,246)'],
 	neon: ['rgb(254,0,0)', 'rgb(253,254,2)', 'rgb(11,255,1)', 'rgb(1,30,254)', 'rgb(254,0,246)'],
-	facebook: ['rgb(59,89,152)', 'rgb(139,157,195)', 'rgb(223,227,238)', 'rgb(247,247,247)', 'rgb(255,255,255)'],
+	facebook: ['rgb(0,0,0)', 'rgb(137,143,156)', 'rgb(66,103,178)'],
 	highlighter: ['rgb(140,255,50)', 'rgb(171,255,50)', 'rgb(212,255,50)', 'rgb(233,255,50)', 'rgb(253,255,50)'],
 }
 
@@ -38,6 +41,10 @@ slider.addEventListener('input', (e) => {
 
 colorThemesDropdown.addEventListener('change', updateColorTheme)
 
+fadeToBlackCheckbox.addEventListener('change', (e) => {
+	fadeToBlack = e.target.checked
+})
+
 function resetSlider() {
 	slider.value = defaultGridSize
 	updateSliderOutput(defaultGridSize)
@@ -48,25 +55,39 @@ function updateSliderOutput(val) {
 }
 function addColor(target) {
 	let rgb
+	// first pass
 	if (!target.style.backgroundColor) {
-		if (chosenColorTheme === 'random') {
-			rgb = getRandomColor()
-		} else {
-			const colorPalette = colorPalettes[chosenColorTheme]
-			rgb = colorPalette[getRandomInt(0, colorPalette.length - 1)]
-		}
+		rgb = getColor(chosenColorTheme)
 		target.dataset.originalColor = rgb
 		target.dataset.passes = 1
+		// subsequent passes
 	} else {
-		target.dataset.passes++
-		const parsedOriginalColor = parseRGB(target.dataset.originalColor)
-		const parsedNewColor = parsedOriginalColor.map((n) => n - (target.dataset.passes - 1) * n * 0.1)
-		rgb = `rgb(${parsedNewColor[0]},${parsedNewColor[1]},${parsedNewColor[2]})`
+		if (fadeToBlack) {
+			target.dataset.passes++
+			const parsedOriginalColor = parseRGB(target.dataset.originalColor)
+			const parsedNewColor = parsedOriginalColor.map((n) => n - (target.dataset.passes - 1) * n * 0.1)
+			rgb = `rgb(${parsedNewColor[0]},${parsedNewColor[1]},${parsedNewColor[2]})`
+		} else {
+			rgb = getColor(chosenColorTheme)
+			target.dataset.passes = 1
+			target.dataset.originalColor = rgb
+		}
 	}
 	target.style.backgroundColor = rgb
 }
+
 function getRandomColor() {
 	return `rgb(${getRandomInt(0, 255)}, ${getRandomInt(0, 255)}, ${getRandomInt(0, 255)})`
+}
+function getColor(theme) {
+	let rgb
+	if (theme === 'random') {
+		rgb = getRandomColor()
+	} else {
+		const colorPalette = colorPalettes[theme]
+		rgb = colorPalette[getRandomInt(0, colorPalette.length - 1)]
+	}
+	return rgb
 }
 function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min)
@@ -119,9 +140,11 @@ function createGrid(n = chosenGridSize) {
 function resetGrid() {
 	chosenColorTheme = defaultColorTheme
 	chosenGridSize = defaultGridSize
+	fadeToBlack = true
 	resetSlider()
 	resetColorThemesDropdown()
 	displayColorTheme()
+	updateFadeToBlackCheckbox()
 	createGrid()
 }
 
@@ -160,5 +183,10 @@ function removeColorTheme() {
 	})
 }
 
+function updateFadeToBlackCheckbox() {
+	fadeToBlackCheckbox.checked = fadeToBlack
+}
+
 createGrid()
 displayColorTheme()
+updateFadeToBlackCheckbox()
